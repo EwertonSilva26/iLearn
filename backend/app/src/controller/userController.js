d7baa82bea435059cdad780b018aed71dc85fbb7
ewertonSoares <ewerton.fa4@gmail.com>
2022-05-30 20:52:56 -0300

    [backend] - adicionando token jwt

--- backend/app/src/controller/userController.js
+++ backend/app/src/controller/userController.js
@@ -1,26 +1,31 @@
 const dbConnection = require("../config/dbserver");
 const connection = dbConnection();
-
 const auth = require("../config/authentication");
+const jwt = require('jsonwebtoken');
+
 const { login, getHash } = require("../model/userModel");
 
 module.exports = {
     loginController: async function (app, req, res) {
-        console.log(`[UserController] - Iniciando login para: ${req.body.email}`)
+        console.log(`[UserController] - Iniciando login: ${req.body.email}`)
 
         const hash = await getUserHash(req.body.email);
-        if(hash == 0) {
-            res.status(403).send({status: 403, message: 'E-mail invalido!' });
+        if (hash == 0) {
+            res.status(400).send({ status: 400, message: 'E-mail invalido!' });
             return;
         }
-        
+
         const isMatch = auth.decryptPassword(req.body.password, hash);
-        if (isMatch ||(hash != 0)) {
+        if (isMatch || (hash != 0)) {
             login(req.body, connection, function (error, result) {
                 if (error) {
-                    res.send({status: 403, registed: false, error });
+                    res.status(403).send({ status: 403, registed: false, error });
                 }
-                res.send({ status: 200, registed: true, result })
+                let id = result.id_user;
+                let token = jwt.sign({ id }, process.env.SECRET, {
+                    expiresIn: 300 // expira in 5min
+                });
+                res.status(200).send({ status: 200, registed: true, token })
             })
         } else {
             throw new Error("Senha incorreta");
--- backend/app/src/model/userModel.js
+++ backend/app/src/model/userModel.js
@@ -3,18 +3,23 @@ let sql;
 
 module.exports = {
 
+  //Faz login de usuario
   login: function (body, connection, callback) {
-    sql = `select count(*) from users where user_email = '${body.email}' and user_pwd = '${body.password}'`;
+    sql = `select id_user from users where user_email = '${body.email}' and user_pwd = '${body.password}'`;
     connection.query(sql, callback);
   },
 
-  getHash: async function (email, connection) {
-    sql = `select user_pwd from users where user_email = '${email}'`;
-    
+  /** Retorna senha hash salva no banco e dados */
+  getHash: async function (email, connection) {    
     let value = await checkEmail(email, connection);
-    if (value <= 0) { return Promise.resolve(value).then(() => { return 0; }); }
+
+    if (value <= 0) { 
+      return Promise.resolve(value)
+      .then(() => { return 0; }); 
+    }
 
     let hash = new Promise((resolve, reject) => {
+      sql = `select user_pwd from users where user_email = '${email}'`;
       connection.query(sql, function (error, result) {
         if (!error) {
           resolve(result[0].user_pwd);
@@ -26,7 +31,7 @@ module.exports = {
     return await hash;
   }
 }
-
+/** Verifica se e-mail existe no banco de dados */
 const checkEmail = async (email, connection) => {
   sql = `select count(*) as value from users where user_email = '${email}'`;
 
