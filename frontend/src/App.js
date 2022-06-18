96a7f940a95750566897f6585b0f18a9fc56904c
ewertonSoares <ewerton.fa4@gmail.com>
2022-06-18 12:04:16 -0300

    Adição de providers no App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
 import './style.css';
-import { BrowserRouter as Router} from 'react-router-dom';
-// import LoginProvider from './context/LoginProvider';
-// import HomeStudentProvider from './context/HomeStudentProvider';
-// import HomeTeacherProvider from './context/HomeTeacherProvider';
+import { BrowserRouter as Router } from 'react-router-dom';
+import LoginProvider from './context/LoginProvider';
+import HomeTeacherProvider from './context/HomeTeacherProvider';
+import HomeStudentProvider from './context/HomeStudentProvider';
 import Classprovider from './context/Classprovider';
-
+import Optionalprovider from './context/OptionalProvider';
+import QuestionProvider from "./context/QuestionProvider";
 import Routes from './routes'
 
 function App() {
   return (
     <Router>
-      <Classprovider>
-        <Routes />
-      </Classprovider>
+      <LoginProvider>
+        <HomeTeacherProvider>
+          <HomeStudentProvider>
+            <Classprovider>
+              <Optionalprovider>
+                <QuestionProvider>
+                <Routes />
+                </QuestionProvider>
+              </Optionalprovider>
+            </Classprovider>
+          </HomeStudentProvider>
+        </HomeTeacherProvider>
+      </LoginProvider>
     </Router>
   );
 }
