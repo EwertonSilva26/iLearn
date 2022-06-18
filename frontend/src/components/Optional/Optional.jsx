13d3f7dd38ce6a57b3b54cd864be637139cbb4c5
ewertonSoares <ewerton.fa4@gmail.com>
2022-06-18 11:57:02 -0300

    Adicionado component de opçao

---
+++ frontend/src/components/Optional/Optional.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { OptionalContext } from "../../context/OptionalProvider";
+
+import "./Optional.css";
+
+const Optional = () => {
+
+    return (
+        <div className="main">
+            <div id="container">
+                <div id="btn_left">
+                    <Link to={"/ansers/"} >
+                    <button>Questoẽs</button>
+                    </Link>
+                </div>
+
+                <div id="btn_rigth">
+                    <Link to={"/students/"} >
+                    <button>Ver alunos</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Optional;
