a832ecf4f7e6f33c49f835fd23c5ea2acaf26b20
ewertonSoares <ewerton.fa4@gmail.com>
2022-06-18 12:05:33 -0300

    Adcionado novas rotas no app.js

--- backend/app/app.js
+++ backend/app/app.js
@@ -4,5 +4,8 @@ const userRoutes = require("../app/routes/routes_user");
 const classRoutes = require("../app/routes/routes_class");
 
 userRoutes.loginUser(app);
+
 classRoutes.createClass(app);
 classRoutes.getClasses(app);
+classRoutes.insertStudentsInClasses(app);
+classRoutes.getStudentClasses(app);
