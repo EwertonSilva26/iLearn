d9962d2785cbbb7a8ad486326b322f1ec6b52c65
ewertonSoares <ewerton.fa4@gmail.com>
2022-06-18 12:08:27 -0300

    Edição classModel

--- backend/app/model/classModel.js
+++ backend/app/model/classModel.js
@@ -11,7 +11,27 @@ module.exports = {
 
   /** Busca todas as turmas **/
   getAllClasses: function (body, connection, callback) {
+    console.log("[MODEL] - Buscando turmas")
     sql = "SELECT * FROM tb_class";
     connection.query(sql, callback);
-  }
+  },
+
+    /** Insere aluno em um turma **/
+    insertStudentInClass: function (body, connection, callback) {
+      console.log("[MODEL] - Buscando turmas")
+      sql = `CALL insert_student_in_class(${body.idStudent}, '${body.classCode}')`;
+      connection.query(sql, callback);
+    },
+
+    /** Busca turmas que o aluno esta cadastrado aluno em um turma **/
+    getStudentClasses: function (body, connection, callback) {
+      console.log("[MODEL] - Buscando turmas")
+      sql = `SELECT * FROM tb_student_teacher_class AS stc
+      LEFT JOIN tb_class AS c
+      ON c.id_class = stc.id_class
+      LEFT JOIN tb_student AS s
+      ON s.id_student = stc.id_student
+      WHERE stc.id_student = ${body.idStudent}`;
+      connection.query(sql, callback);
+    }
 }
